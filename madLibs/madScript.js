dc8656c59e999169b09e03d6aed0bab9444f6809
Daniel Ramos <phyzekz@scc2102-07.ucdavis.edu>
2023-02-09 16:42:51 -0800

    mostly just edited the madLibs css/js/html

--- madLibs/madScript.js
+++ madLibs/madScript.js
@@ -5,5 +5,18 @@ console.log("Run Forrest JS run");
 
 const inp = querySelector("input");
 
+// let play = document.getElementById("play");
+// function playMusic(){
+
+// let audio = new Audio("01 - Prologue.mp3");
+
+// }
+// play.addEventListener("click", playMusic);
+
+const aud = document.getElementById("myAudio");
+
+function playAud() {
+    aud.play();
+}
 
 })();
