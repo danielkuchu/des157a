fd91f61fed49d3e60593d57a3c0992a6b88caac5
Daniel Ramos <you@example.com>
2023-02-13 23:55:01 -0800

    Additions to css and JS files

--- madLibs/madScript.js
+++ madLibs/madScript.js
@@ -1,17 +1,45 @@
-(function(){
+(function () {
 
-"use strict";
-console.log("Run Forrest JS run");
+    "use strict";
+    console.log("Run Forrest JS run");
 
-const inp = querySelector("input");
+    const inp = document.querySelector("input");
+    const play = document.getElementById("play");
+    const myForm = document.querySelector('#myform');
+    const madlib = document.querySelector('#madlib');
 
-const play = document.getElementById("play");
-function playMusic(){
 
-const audio = new Audio("hpsong.mp3");
+    function playMusic() {
+        console.log("Music has started");
+        const audio = new Audio("hpsong.mp3");
+        audio.play();
 
-}
-play.addEventListener("click", playMusic);
+    }
+
+    play.addEventListener("click", playMusic);
+
+
+    myForm.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+        const adj1 = document.querySelector("#adj1").value;
+        const tod = document.querySelector("#tod").value;
+        const yname = document.querySelector("#yname").value;
+        const profs = document.querySelector("#profs").value;
+        const mean = document.querySelector("#mean").value;
+        const verb1 = document.querySelector("#verb1").value;
+        const place = document.querySelector("#place").value;
+        const spell = document.querySelector("#spell").value;
+        const dance = document.querySelector("#dance").value;
+        const myText = ``
+
+
+
+
+
+    });
+
+
 
 
 
