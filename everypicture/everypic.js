29f4a3c5bdacd3075b64fbc1bd1655a5329599fb
Daniel Ramos <phyzekz@scc2102-07.ucdavis.edu>
2023-02-28 17:01:55 -0800

    everypic

--- everypicture/everypic.js
+++ everypicture/everypic.js
@@ -3,8 +3,7 @@
     "use strict";
     console.log("JS is running faster than the offspring of Forrest Gump and Usain Bolt.");
 
-    const container = document.querySelector("#floor");
-    const hotSpots = document.querySelectorAll("#floor article");
+    const hotSpots = document.querySelectorAll("#main article");
     const theImg = document.querySelector("article img");
 
     hotSpots.forEach(function (eachSpot) {
@@ -17,6 +16,7 @@
 
         const thisCorner = event.target.id;
         console.log(thisCorner);
+
         switch (thisCorner) {
 
             case "egg": theImg.className = "egg"; break;
@@ -39,6 +39,17 @@
         });
 
    } 
+
+   //overlays
+   
+   document.querySelector(".open").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("#overlay").className = "showing";
+
+});
+document.querySelector(".closed").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("#overlay").className = "hidden";});
 
 
 })();
