7e78876407e2933c431ea871025b0678cf8ae54a
Daniel Ramos <you@example.com>
2023-03-06 01:58:21 -0800

    GameOn halfway done

--- gameOn/game.js
+++ gameOn/game.js
@@ -3,6 +3,15 @@
 "use strict";
 console.log("Jesus is running")
 
+document.querySelector("#startgame").addEventListener("click",function(){
 
+    document.getElementById("cloud1").className="hide";
+    document.getElementById("cloud2").className="hide";
+    document.getElementById("bush1").className="hide";
+    document.getElementById("bush2").className="hide";
+    document.getElementById("bush3").className="hide";
+    document.getElementById("startgame").className="hide";
+    document.getElementById("title").className="hide";
+});
 
 })();
