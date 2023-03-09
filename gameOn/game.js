ae6c86d8898ef2d42f0c5e3f5f52b07d5450bb22
Daniel Ramos <you@example.com>
2023-03-09 03:08:31 -0800

    almost finished gameOn, just need to add 2nd health bar and js for losing conditions/switching turns

--- gameOn/game.js
+++ gameOn/game.js
@@ -4,24 +4,44 @@
     console.log("Jesus is running")
 
     //change to match elements within game
-    // bar1, bar2, text, and roll button, begin button
+    // bar1 done, bar2, text done, and roll button done, begin button done
 
 
     const startGame = document.getElementById('startgame');
+    const roll = document.getElementById("roll");
     const gameControl = document.getElementById('gamecontrol');
-    const score = document.getElementById('score');
-    const textbox = document.getElementsByTagName('texbox p');
-    let health = document.getElementById("health");
-    health.value -= 50;
+    const textbox = document.getElementById('texbox');
+    const rules = document.querySelector("#rules");
+    const ruleLay = document.querySelector("#rulesOverlay");
+    const closeRules = document.querySelector("#closeOverlay");
+    let healthBar1 = document.getElementById("health1");
 
     //Starting screen to page 2
 
-    document.querySelector("#startgame").addEventListener("click", function () {
+   startGame.addEventListener("click", function () {
 
         document.getElementById("page1").className = "hide";
         document.getElementById("page2").className = "show";
 
     });
+
+    //Opening rules overlay
+
+    rules.addEventListener("click", function (event) {
+        event.preventDefault();
+        ruleLay.className = "show";
+
+
+    });
+
+    //closing rules overlay
+
+    closeRules.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("Closing Rules Overlay");
+        ruleLay.className = "hide";
+    });
+
 
     //change object
 
@@ -36,7 +56,7 @@
             "`${gameData.players[gameData.index]} tapped into their secret steroid stash! HP slightly restored`", //+10 hp
             "`${gameData.players[gameData.index]} charged up for a game-ending attack!.. But it missed.`", // Turn skipped -- no effect
             "`${gameData.players[gameData.index]} feels a little shy... Turn skipped`", // Turn skipped
-            "`${gameData.players[gameData.index]} got confused and attacked themself!`", // -5 hp
+            "`${gameData.players[gameData.index]} got confused and attacked themself!`", // -15 hp
 
         ],
         roll: 0,
@@ -50,6 +70,7 @@
         console.log(gameData.index);
 
         gameControl.innerHTML = '<h2>The Game Has Started</h2>';
+        setTimeout(setUpTurn, 2000);
 
 
 
@@ -57,9 +78,9 @@
     });
 
     function setUpTurn() {
-        textbox.innerHTML = `<p>Roll for an attack, ${gameData.players[gameData.index]}</p>`;
-    //    textbox.innerHTML = '<button id="roll">Roll the Dice</button>';
-        document.getElementById('roll').addEventListener('click', function () {
+        gameControl.innerHTML = `<p>Roll for an attack, ${gameData.players[gameData.index]}</p>`;
+        //    textbox.innerHTML = '<button id="roll">Roll the Dice</button>';
+        roll.addEventListener('click', function () {
 
             throwDice();
 
@@ -67,10 +88,9 @@
     }
 
     function throwDice() {
-        actionArea.innerHTML = '';
         gameData.roll = Math.floor(Math.random() * 7) + 1; //using ceil could result in a zero
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML += `<img src="${gameData.dice[gameData.roll - 1]}"> `; // detting inner green bar to the width (player's health)
+        gameControl.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        // setting inner green bar to the width (player's health)
         // if (gameData.index == 0){ 
         // bar.style.width= `${gameData.score[gameData.index]}px`;
 
@@ -79,60 +99,72 @@
         ;
 
 
-        // if two 1's are rolled...
-        if (gameData.rollSum === 2) {
-            game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
-            gameData.score[gameData.index] = 0;
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            showCurrentScore();
+        // if 1 is rolled... Weak Attack
+        if (gameData.roll === 0) {
+
+            healthBar1.value -= 10;
+            gameData.messages[gameData.index]=0;
             setTimeout(setUpTurn, 2000);
         }
 
-        // if either die is a 1...
-        else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to  ${gameData.players[gameData.index]
-                }</p>`;
+        // if 2 is rolled... Strong Attack
+        else if (gameData.roll === 1) {
+            gameData.messages[gameData.index]=1;
+            healthBar1.value -= 25;
             setTimeout(setUpTurn, 2000);
         }
 
-        // if neither die is a 1...
+// If 3 is rolled, turn skipped
+    
+        else if (gameData.roll === 2) {
+            gameData.messages[gameData.index]=2;
+            setTimeout(setUpTurn, 2000);
+        }
+
+        //if 4 is rolled... +10 hp
+
+        else if (gameData.roll === 3) {
+            gameData.messages[gameData.index]=3;
+            healthBar1.value += 10;
+            setTimeout(setUpTurn, 2000);
+        }
+
+        //if 5 is rolled... turn is skipped
+
+
+        else if (gameData.roll === 4) {
+            gameData.messages[gameData.index]=4;
+            setTimeout(setUpTurn, 2000);
+        }
+
+        //if 6 is rolled... turn is skipped
+
+        else if (gameData.roll === 5) {
+            gameData.messages[gameData.index]=5;
+            setTimeout(setUpTurn, 2000);
+        }
+
+
+        //if 7 is rolled, user damages self -15 hp
+        else if (gameData.roll === 6) {
+            gameData.messages[gameData.index]=6;
+            healthBar1.value -= 15;
+            setTimeout(setUpTurn, 2000);
+        }
+
         else {
-            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
 
-            document.getElementById('rollagain').addEventListener('click', function () {
-                //setUpTurn();
-                throwDice();
-            });
-
-            document.getElementById('pass').addEventListener('click', function () {
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
-
-            checkWinningCondition();
+            checkLosingCondition();
         }
 
     }
 
-    function checkWinningCondition() {
-        if (gameData.score[gameData.index] > gameData.gameEnd) {
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} 
-        wins with ${gameData.score[gameData.index]} points!</h2>`;
+    function checkLosingCondition() {
+        if (gameData.score[gameData.index] < gameData.gameEnd) {
+            gameControl.innerHTML = `<h2>${gameData.players[gameData.index]} 
+        has fainted!</h2>`;
 
-            actionArea.innerHTML = '';
             document.getElementById('quit').innerHTML = 'Start a New Game?';
-        } else {
-            // show current score...
-            showCurrentScore();
-        }
-    }
-
-    // function showCurrentScore() {
-    //     score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}
-    // ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} 
-    // ${gameData.score[1]}</strong></p>`;
-    // }
+        }}
 
 })();
